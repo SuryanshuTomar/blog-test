d2a41cb07654dd83bbd6161ed274d35fc60abe65
Suryanshu Tomar <suryanshutomar123@outlook.com>
2023-12-24 15:52:21 +0530

    refactor: minor

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(
 	cors({
-		origin: ["http://localhost:5173", "https://blog-test-cient.vercel.app/"],
+		origin: ["http://localhost:5173", "https://blog-test-cient.vercel.app"],
 		credentials: true,
 	})
 );
